3d74434885293255c73c3109741c74f6f1e8a8b2
Adham EL Banhawy <the.benhawy@gmail.com>
2018-02-19 22:00:20 -0600

    Mapped over contests to dispay in separate preview component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,17 @@
 import React, { Component } from 'react'
 import Header from './Header.jsx'
+import ContestPreview from './ContestPreview.jsx'
 
 class App extends Component {
     state = {
-        pageHeader: 'Naming Contests'
+        pageHeader: 'Naming Contests',
     }
     render() {
         return(
             <div className="App">
                 <Header message={this.state.pageHeader}/>
                 <div>
-                    ...
+                {this.props.contests.map(contest => <ContestPreview {...contest}/>)}  
                 </div>
             </div>
         )
--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom'
 import App from './components/App.jsx'
 import data from "./testData.json";
 
-console.log(data)
-
 ReactDOM.render(
     <App contests={data.contests} />,
     document.getElementById('root')
