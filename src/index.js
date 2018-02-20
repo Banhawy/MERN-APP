3460015526bd4db9a891b981bb72a3f00637259a
Adham EL Banhawy <the.benhawy@gmail.com>
2018-02-19 18:36:44 -0600

    used ES6 class

--- src/index.js
+++ src/index.js
@@ -1,23 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
+import App from './components/App'
 
-const Header = (props) => {
-    return(
-        <h2 className='text-center'>
-            {props.message}
-            </h2>
-    )
-}
-const App = () => {
-    return(
-        <div>
-            <Header message="Naming Contest"/>
-            <div>
-                ...
-            </div>
-        </div>
-    )
-}
 ReactDOM.render(
     <App />,
     document.getElementById('root')
