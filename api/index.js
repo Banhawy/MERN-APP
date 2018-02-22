cae1de4f5fe3e3f2a5c4fd61da15fc51cc7fd342
Adham EL Banhawy <the.benhawy@gmail.com>
2018-02-21 19:06:07 -0600

    Integrated mongoDB with test data

--- api/index.js
+++ api/index.js
@@ -1,22 +1,45 @@
 import express from 'express'
-import data from '../src/testData.json'
-import { request } from 'http';
+import { MongoClient } from 'mongodb'
+import assert from 'assert'
+import config from '../config'
+
+let mdb;
+MongoClient.connect(config.mongodbUri, (err, database) => {
+    //raise an error if error exists
+    assert.equal(null, err)
+
+    mdb = database.db('test')
+}) 
+
 const router = express.Router()
-const contests = data.contests.reduce((obj, contest) => {
-    obj[contest.id] = contest;
-    return obj;
-}, {})
 
 router.get('/contests', (req, res)=>{
-    res.send({ 
-        contests: contests
-    })
+    let contests = {}
+    mdb.collection('contests').find({})
+        .project({
+            id: 1,
+            categoryName: 1,
+            contestName: 1
+        })
+        .each((err, contest) => {
+            assert.equal(null, err)
+
+            if (!contest){
+                res.send({contests})
+                return
+            }
+
+            contests[contest.id] = contest 
+        })
 })
 
 router.get('/contests/:contestId', (req, res)=>{
-    let contest = contests[req.params.contestId]
-    contest.description = 'Lorem ipsum'
-    res.send(contest)
+    mdb.collection('contests')
+        .findOne({
+            id: Number(req.params.contestId)
+        })
+        .then(contest => res.send(contest))
+        .catch(console.error)
 })
 
 export default router
