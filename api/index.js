752d6cbe67ea073a66f204ebf51e00d0261cbec0
Adham EL Banhawy <the.benhawy@gmail.com>
2018-02-21 20:23:45 -0600

    Added new api for fetching contest suggested names

--- api/index.js
+++ api/index.js
@@ -13,26 +13,22 @@ MongoClient.connect(config.mongodbUri, (err, database) => {
 
 const router = express.Router()
 
-router.get('/contests', (req, res)=>{
-    let contests = {}
-    mdb.collection('contests').find({})
-        .project({
-            id: 1,
-            categoryName: 1,
-            contestName: 1
-        })
-        .each((err, contest) => {
+router.get('/names/:nameIds', (req, res)=>{
+    const nameIds = req.params.nameIds.split(',').map(Number)
+    let names = {}
+    mdb.collection('names').find({ id: {$in: nameIds}})
+        .each((err, name) => {
             assert.equal(null, err)
 
-            if (!contest){
-                res.send({contests})
+            if (!name){
+                res.send({names})
                 return
             }
 
-            contests[contest.id] = contest 
+            names[name.id] = name 
         })
 })
-
+ 
 router.get('/contests/:contestId', (req, res)=>{
     mdb.collection('contests')
         .findOne({
